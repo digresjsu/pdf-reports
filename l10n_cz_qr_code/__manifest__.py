44838474b7fe38e61918a5134dabae0c6a234222
JSU <jsu@digres.cz>
2026-02-12 10:42:41 +0100

    qr code

---
+++ l10n_cz_qr_code/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Digital Resources",
+
+    'summary': """
+        Czech localization for QR codes""",
+
+    'description': """
+        Adds the option to generate QR codes for payments in the Czech Republic using the QR platba codes.
+    """,
+    'license': 'LGPL-3',
+    'author': "Digital Resources a.s.",
+    'website': "https://www.digres.cz",
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'category': 'Accounting/Localization',
+    'version': '0.0.3',
+
+    'depends': ['base','account'],
+}
